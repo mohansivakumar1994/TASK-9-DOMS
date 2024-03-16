90a8aa756790476a27eff915eb87b77b43f7bb35
MOHAN SIVAKUMAR <139428429+mohansivakumar1994@users.noreply.github.com>
2024-03-17 01:20:18 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+1) Pagination in DOM Manipulation
+
+click to below the html view this answer pagination in dom manipulation
+http://127.0.0.1:5500/Dom%20manipulation%20and%20form/Calculator/Manipulation%20task/index.html
+
+
+
+2) Calculator Task
+
+click to below the html view this answer calculator
+http://127.0.0.1:5500/Dom%20manipulation%20and%20form/Calculator/index.html
+
+
+3) DOM Manipulation with forms
+  
+   click to below the html view this answer DOM Manipulation with forms
+http://127.0.0.1:5500/Dom%20manipulation%20and%20form/index.html
--- script.js
+++ script.js
@@ -1,76 +1,162 @@
-function submitForm() {
- let firstNameInput = document.getElementById("first-name");
- let lastNameInput = document.getElementById("last-name");
- let addressInput = document.getElementById("address");
- let pincodeInput = document.getElementById("pincode");
- let genderInputs = document.getElementsByName("gender");
- let foodChoiceInput = document.getElementById("food-choice");
- let stateInput = document.getElementById("state");
- let countryInput = document.getElementById("country");
+//Fetch Task todo data
+fetch("https://jsonplaceholder.typicode.com/todos").then((res) => {
+    res.json().then((data) => {
+        objJson = data;
+        changePage(1);
+    })
+})
 
- // Retrieve values from the input fields
- let firstName = firstNameInput.value;
- let lastName = lastNameInput.value;
- let address = addressInput.value;
- let pincode = pincodeInput.value;
- let gender = getSelectedRadioValue(genderInputs);
- let foodChoice = foodChoiceInput.value;
- let state = stateInput.value;
- let country = countryInput.value;
-
- // Validate if first name and last name are provided
- if (firstName && lastName) {
-     // Create a new row in the table
-     let table = document.getElementById("dataTable");
-     let newRow = table.insertRow(table.rows.length);
-
-     // Insert cells with data
-     let cell1 = newRow.insertCell(0);
-     let cell2 = newRow.insertCell(1);
-     let cell3 = newRow.insertCell(2);
-     let cell4 = newRow.insertCell(3);
-     let cell5 = newRow.insertCell(4);
-     let cell6 = newRow.insertCell(5);
-     let cell7 = newRow.insertCell(6);
-     let cell8 = newRow.insertCell(7);
-
-     // Populate cells with input values
-     cell1.innerHTML = firstName;
-     cell2.innerHTML = lastName;
-     cell3.innerHTML = address;
-     cell4.innerHTML = pincode;
-     cell5.innerHTML = gender;
-     cell6.innerHTML = foodChoice;
-     cell7.innerHTML = state;
-     cell8.innerHTML = country;
-
-     // Clear form inputs
-     firstNameInput.value = "";
-     lastNameInput.value = "";
-     addressInput.value = "";
-     pincodeInput.value = "";
-     resetRadioButtons(genderInputs);
-     foodChoiceInput.value = "";
-     stateInput.value = "";
-     countryInput.value = "";
- } else {
-     alert("Please fill in all required fields...");
- }
+//Create Elements and set properties classname , Id, name
+function createDomElement(elem, elemClass = '', elemName = '', elemId = '') {
+    let element = document.createElement(elem);
+    (elemClass !== '') && element.setAttribute('class', elemClass);
+    (elemName !== '') && element.setAttribute('name', elemName);
+    (elemId !== '') && element.setAttribute('id', elemId);
+    return element;
 }
 
-// Helper function to get the value of the selected radio button
-function getSelectedRadioValue(radioInputs) {
- for (let i = 0; i < radioInputs.length; i++) {
-     if (radioInputs[i].checked) {
-         return radioInputs[i].value;
-     }
- }
- return null; // Return null if no radio button is selected
+//Create prev button
+let prevBtn = createDomElement('button', 'btn-navigate btn-prev pagination-col', 'btn-prev');
+prevBtn.innerText = 'prev';
+prevBtn.addEventListener('click', function () {
+    prevPage();
+})
+
+//Create next btn
+let nextBtn = createDomElement('button', 'btn-navigate btn-next pagination-col', 'btn-next');
+nextBtn.innerText = 'next';
+nextBtn.addEventListener('click', function () {
+    nextPage();
+})
+
+//Create Pagination container
+let paginationContainer = createDomElement('div', 'pagination-container');
+function createPagination() {
+    paginationContainer.append(getPaginationElements());
+    return paginationContainer;
 }
 
-// Helper function to reset radio buttons
-function resetRadioButtons(radioInputs) {
- for (let i = 0; i < radioInputs.length; i++) {
-     radioInputs[i].checked = false;
- }
+//Creating a grid container to show the values
+let displayGridContainer = createDomElement('div', 'display-grid-table');
+
+
+//Create a grid row and return
+function getDisplayGridRow() {
+    let displayGridRow = createDomElement('div', 'display-grid-row');
+    return displayGridRow;
+}
+
+//Create a grid column and return
+function getDisplayGridColumn() {
+    let displayGridColumn = createDomElement('div', 'display-grid-column');
+    return displayGridColumn;
+}
+
+//Create table with column title
+addValuesToTheTable('First name', 'Last name', 'Address', 'Pincode');
+
+//Creating and adding values to the table 
+function addValuesToTheTable(col1, col2, col3, col4) {
+    let row = getDisplayGridRow();
+    let column1 = getDisplayGridColumn();
+    column1.innerHTML = col1;
+    let column2 = getDisplayGridColumn();
+    column2.innerHTML = col2;
+    let column3 = getDisplayGridColumn();
+    column3.innerHTML = col3;
+    let column4 = getDisplayGridColumn();
+    column4.innerHTML = col4;
+    row.append(column1, column2, column3, column4);
+    displayGridContainer.append(row);
+}
+
+//Create pagination column
+function getPaginationCol() {
+    let paginationColumn = createDomElement('div', 'pagination-col');
+    return paginationColumn;
+}
+
+//Create pagination row
+function getPaginationRow() {
+    let paginationRow = createDomElement('div', 'pagination-row');
+    return paginationRow;
+}
+
+//Create pagination Element
+function getPaginationElements() {
+    let paginationRow = getPaginationRow();
+    paginationRow.append(prevBtn);
+    for (let i = 1; i < numPages(); i++) {
+        let paginationCol = getPaginationCol();
+        paginationCol.innerText = i;
+        paginationCol.setAttribute('onclick', `changePage(${i})`);
+        if (currentPage === i) {
+            paginationCol.setAttribute('class', 'pagination-col active-page');
+        }
+        paginationRow.append(paginationCol);
+    }
+    paginationRow.append(nextBtn);
+    return paginationRow;
+}
+
+//Set Default Values
+var currentPage = 1;
+var recordsPerPage = 10;
+
+//Creating prev page action 
+function prevPage() {
+    if (currentPage > 1) {
+        currentPage--;
+        changePage(currentPage);
+    }
+}
+
+//Creating next page action
+function nextPage() {
+    if (currentPage < numPages()) {
+        currentPage++;
+        changePage(currentPage);
+    }
+}
+
+//Display data on page change
+function changePage(page) {
+
+    // Validate page
+    if (page < 1) {
+        page = 1;
+    } else if (page > numPages()) {
+        page = numPages();
+    }
+
+    currentPage = page;
+
+    //Empty the table and set the column names
+    displayGridContainer.innerHTML = '';
+    paginationContainer.innerHTML = '';
+    addValuesToTheTable('Id', 'Task Title', 'User Id', 'Task Completed');
+
+    //Add data in the column
+    for (var i = (page - 1) * recordsPerPage; i < (page * recordsPerPage); i++) {
+        addValuesToTheTable(objJson[i].id, objJson[i].title, objJson[i].userId, objJson[i].completed);
+    }
+
+    // Show and hide prev/next button
+    if (page == 1) {
+        prevBtn.style.visibility = "hidden";
+    } else {
+        prevBtn.style.visibility = "visible";
+    }
+    if (page == numPages()) {
+        nextBtn.style.visibility = "hidden";
+    } else {
+        nextBtn.style.visibility = "visible";
+    }
+    //Append table and pagination
+    document.body.append(displayGridContainer, createPagination());
+}
+
+//Get total number of pages
+function numPages() {
+    return Math.ceil(objJson.length / recordsPerPage);
 }
